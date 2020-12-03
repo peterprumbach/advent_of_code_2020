f30fc7c80a3ef72bdd7236fe2dc2e1a3166cf949
Peter Prumbach <34021473+peterprumbach@users.noreply.github.com>
2020-12-03 11:40:42 +0100

    changes

--- 3/3.py
+++ 3/3.py
@@ -1,17 +1,17 @@
 import numpy as np
 map = []
+count = 0
+position = 0
 
 #Open map file
 with open('3/map') as file:
     map = file.readlines()
 
-count = 0
-position = 0
-
+#Iterate
 for i in range(0, len(map), 1):
     row = map[i][:-1]
     if('#' == row[position%len(row)]):
         count+=1
     position += 3
 
-print("part1: ", count)
+print(count)
