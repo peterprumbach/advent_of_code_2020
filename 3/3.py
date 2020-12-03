8aa65429a7290ebe2e08889e760f5bdc705b4a9a
Peter Prumbach <34021473+peterprumbach@users.noreply.github.com>
2020-12-03 11:39:52 +0100

    Uploaded solution

--- 3/3.py
+++ 3/3.py
@@ -1,12 +1,17 @@
 import numpy as np
 map = []
 
-#right, down
-step=[3,1]
-
 #Open map file
 with open('3/map') as file:
-    map = list(file.read().splitlines())
+    map = file.readlines()
 
-#Expand map
+count = 0
+position = 0
 
+for i in range(0, len(map), 1):
+    row = map[i][:-1]
+    if('#' == row[position%len(row)]):
+        count+=1
+    position += 3
+
+print("part1: ", count)
